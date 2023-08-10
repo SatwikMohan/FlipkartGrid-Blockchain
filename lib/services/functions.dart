5b5d34fcbfa94ded509e2e590bcf31212bed04d0
SatwikMohan <satwikplay69@gmail.com>
2023-08-10 15:48:38 +0530

    block chain started

--- lib/services/functions.dart
+++ lib/services/functions.dart
@@ -5,7 +5,7 @@ import 'package:web3dart/web3dart.dart';
 Future<DeployedContract> loadContract() async{
   String abi=await rootBundle.loadString('assets/abi.json');
   String contractAddress=contractAddress1;
-  final contract=DeployedContract(ContractAbi.fromJson(abi, 'Election'), EthereumAddress.fromHex(contractAddress));
+  final contract=DeployedContract(ContractAbi.fromJson(abi, 'FungibleToken'), EthereumAddress.fromHex(contractAddress));
   return contract;
 }
 
@@ -16,4 +16,17 @@ Future<String> callFunction(String functionName,List<dynamic> args,Web3Client et
   final result=await ethClient.sendTransaction(credential, Transaction.callContract(contract: contract, function: ethFunction, parameters: args)
       ,chainId: null,fetchChainIdFromNetworkId: true);
   return result;
+}
+
+Future<String> addCustomer(String name,String email,String password,String customerAddress,Web3Client ethClient){
+  var response=callFunction('addCustomer', [name,email,password,customerAddress], ethClient, owner_private_key);
+  print('Function addCustomer Called Successfully');
+  return response;
+}
+
+String getUserData(String email,Web3Client ethClient){
+  var response=callFunction('getUserData', [email], ethClient, owner_private_key);
+  print('Function getUserData Called Successfully');
+  print(response);
+  return response.toString();
 }
