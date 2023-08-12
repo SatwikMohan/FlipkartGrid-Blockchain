f2fa0237c968af2aace53748806332896629b53a
SatwikMohan <satwikplay69@gmail.com>
2023-08-12 16:13:22 +0530

    block chain

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
     // SizeConfig().init(context);
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
