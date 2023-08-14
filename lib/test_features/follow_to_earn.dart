1fcb9229e4752d3673c3bc33afcc2dbbc346a98d
AbhinavISM <111358311+AbhinavISM@users.noreply.github.com>
2023-08-15 00:08:17 +0530

    rewards on social media follow

--- lib/test_features/follow_to_earn.dart
+++ lib/test_features/follow_to_earn.dart
@@ -1,5 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flipgrid/main.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:web3dart/web3dart.dart';
+
+import '../services/functions.dart';
+import '../utils/constants.dart';
 
 Future<void> _launchSocialMediaAppIfInstalled({
   required String url,
@@ -20,7 +28,6 @@ const List<String> socialMediaUrls = [
   "https://www.facebook.com/avinave.agrwal/",
   "https://www.facebook.com/avinave.agrwal/",
   "https://www.facebook.com/avinave.agrwal/",
-  "https://www.facebook.com/avinave.agrwal/"
 ];
 const List<String> socialMediaPlatforms = ["Instagram", "Facebook", "Twitter"];
 
@@ -49,8 +56,23 @@ class _FollowToEarnState extends State<FollowToEarn> {
   }
 }
 
-class FollowOnSocials extends StatelessWidget {
+class FollowOnSocials extends StatefulWidget {
   const FollowOnSocials({super.key});
+
+  @override
+  State<FollowOnSocials> createState() => _FollowOnSocialsState();
+}
+
+class _FollowOnSocialsState extends State<FollowOnSocials> {
+  Client? client;
+  Web3Client? ethClient;
+
+  @override
+  void initState() {
+    client = Client();
+    ethClient = Web3Client(infura_url, client!);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,12 +86,56 @@ class FollowOnSocials extends StatelessWidget {
             itemCount: 3,
             itemBuilder: (context, index) {
               return Column(children: [
-                ElevatedButton(
-                  onPressed: () => {
-                    _launchSocialMediaAppIfInstalled(
-                        url: socialMediaUrls[index])
+                Consumer(
+                  builder:
+                      (BuildContext context, WidgetRef ref, Widget? child) {
+                    final user =
+                        ref.read(currentUserStateProvider).getCurrentUser;
+                    return ElevatedButton(
+                      onPressed: () async {
+                        _launchSocialMediaAppIfInstalled(
+                            url: socialMediaUrls[index]);
+                        if (user.instaFollowed && index == 0) {
+                          ref.read(currentUserStateProvider).setCurrentUser =
+                              ref
+                                  .read(currentUserStateProvider)
+                                  .getCurrentUser
+                                  .copyWith(instaFollowed: true);
+                          ServiceClass().mintDailyCheckInLoyaltyPoints(
+                              user.customerAddress, ethClient!);
+                          await FirebaseFirestore.instance
+                              .collection("Customers")
+                              .doc(user.email)
+                              .set(user.toJson());
+                        } else if (user.fbFollowed && index == 1) {
+                          ref.read(currentUserStateProvider).setCurrentUser =
+                              ref
+                                  .read(currentUserStateProvider)
+                                  .getCurrentUser
+                                  .copyWith(fbFollowed: true);
+                          ServiceClass().mintDailyCheckInLoyaltyPoints(
+                              user.customerAddress, ethClient!);
+                          await FirebaseFirestore.instance
+                              .collection("Customers")
+                              .doc(user.email)
+                              .set(user.toJson());
+                        } else if (user.twitterFollowed && index == 2) {
+                          ref.read(currentUserStateProvider).setCurrentUser =
+                              ref
+                                  .read(currentUserStateProvider)
+                                  .getCurrentUser
+                                  .copyWith(twitterFollowed: true);
+                          ServiceClass().mintDailyCheckInLoyaltyPoints(
+                              user.customerAddress, ethClient!);
+                          await FirebaseFirestore.instance
+                              .collection("Customers")
+                              .doc(user.email)
+                              .set(user.toJson());
+                        }
+                      },
+                      child: Text(socialMediaPlatforms[index]),
+                    );
                   },
-                  child: Text(socialMediaPlatforms[index]),
                 ),
                 const Padding(padding: EdgeInsets.only(top: 8)),
               ]);
